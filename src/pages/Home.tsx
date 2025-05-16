ff01f96e0658ebf3cc4089cd4a5e2394a88b11c4
Thiago perrut <thi.perrut@gmail.com>
2025-05-16 00:10:55 -0300

    change footer home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -278,6 +278,7 @@ const Home: React.FC = () => {
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
             <div>
+              <h3 className="text-xl font-bold mb-4"> Plataforma AltarHub</h3>
               <div className="flex items-center gap-3 mb-4">
                 <div className="bg-white rounded-full p-1 flex items-center justify-center">
                   <svg width="32" height="32" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-church-blue">
@@ -315,7 +316,7 @@ const Home: React.FC = () => {
             </div>
           </div>
           <div className="border-t border-gray-700 pt-4 text-center text-gray-400">
-            <p>&copy; 2025 Plataforma <span className="font-bold text-church-red">AltarHub</span>. Todos os direitos reservados.</p>
+            <p>&copy; 2025 Plataforma  Plataforma AltarHub<span className="font-bold text-church-red">AltarHub</span>. Todos os direitos reservados.</p>
           </div>
         </div>
       </footer>
