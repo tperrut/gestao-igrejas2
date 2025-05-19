97cc38d1d70a9388e02b22d09b45eacd3454835f
Thiago Perrut <thi.perrut@gmail.com>
2025-05-18 22:11:21 -0300

    change logo blue background

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -22,18 +22,13 @@ export const Logo: React.FC<LogoProps> = ({
     <div className={`flex items-center gap-2 ${className}`}>
       <div className={`${sizeClasses[size]} relative`}>
         <img 
-          src="/lovable-uploads/5b587a7e-6cfb-401a-9bf4-17e540f0f7bf.png" 
+          src="https://gecwfcexufkvdkndbjip.supabase.co/storage/v1/object/sign/lp-altar-hub/logo2_com_texto%20(1).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5XzZiZmIxZTU3LTU0MTMtNGUyNi1iZTkwLTc2MDU5MDM4NTdmNyJ9.eyJ1cmwiOiJscC1hbHRhci1odWIvbG9nbzJfY29tX3RleHRvICgxKS5wbmciLCJpYXQiOjE3NDc2MTcwMjAsImV4cCI6MTc3OTE1MzAyMH0.4_kzLZN7hfa9Eudx8O3VOghGVJpfQcQp9tfA9HFmKYA" 
           alt="AltarHub Logo" 
           className="w-full h-full object-contain"
         />
       </div>
       
-      {withText && (
-        <div className="font-bold">
-          <span className="text-church-red">Altar</span>
-          <span className="text-church-blue">Hub</span>
-        </div>
-      )}
+      
     </div>
   );
 };
