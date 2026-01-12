9f708938a120929f764758a207e135a45faf85a7
Thiago Perrut <thi.perrut@gmail.com>
2026-01-12 00:23:30 -0300

    fix: lint3

--- eslint.config.js
+++ eslint.config.js
@@ -4,35 +4,37 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default {
-  root: true,
-  parser: tsParser,
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: ['./tsconfig.app.json', './tsconfig.node.json'],
+export default [
+  {
+    ignores: ['dist'],
   },
-  plugins: {
-    '@typescript-eslint': tsPlugin,
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  rules: {
-    // React Hooks
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      // React Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
 
-    // React Refresh
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // React Refresh
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
-    // TypeScript
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unused-expressions': 'off',
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
   },
-};
+];
